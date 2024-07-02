842503433832bee0bbb2c707183ce2ccfa70873f
dacaiguoguo <dacaiguoguo@163.com>
2024-07-02 20:05:05 +0800

    Initial Commit

---
+++ getCert/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  getCert
+//
+//  Created by yanguo sun on 2024/7/2.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
